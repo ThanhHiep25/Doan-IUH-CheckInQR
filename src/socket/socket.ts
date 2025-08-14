6978e3357add6b12094b2d07ccd5f0c90d438e08
Nguyễn Hiệp <hiepnguyen.250402@gmail.com>
2025-08-14 18:28:07 +0700

    update UI  Zz:14:08:25

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Lấy URL cơ sở từ biến môi trường
-const API_BASE_URL ="https://checkin-iuh-backend.onrender.com/";
+const API_BASE_URL ="https://checkin-iuh-backend.onrender.com";
 
 // Tạo một instance Socket.io duy nhất
 export const socket = io(API_BASE_URL);
